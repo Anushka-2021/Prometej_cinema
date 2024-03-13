7037f596bc91483275fb7a208844e3012df9c5e9
apollliinaria <polinaandreeva0402@gmail.com>
2024-03-13 21:12:29 +0300

    add styles

--- index.php
+++ index.php
@@ -16,13 +16,13 @@ else if(isset($_SESSION['signin_login'])){
                 <meta charset='utf-8'>
                 <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
                 <title>Добро пожаловать!</title>
+                <link href='css/style.css' media='screen' rel='stylesheet'>
             </head>
             <body>
-                <h1 id='t1'>Добро пожаловать в наш кинотеатр!</h1>
-                <a href='http://kr8/registration'>Add new worker</a>
-                <a href='https://kr8/movies/' id='t3'>Выбрать фильм</a>
-                <a href='https://kr8/show_tickets/' id='t3'>Посмотреть проданные билеты</a>
-                <a href='?exit=true'>Выйти</a>
+                <h1 id='t1'>Корпоративный сайти кинотеатра Prometheus</h1>
+                <a class='button' href='https://kr8/movies/' id='t3'>Выбрать фильм</a>
+                <a class='button' href='https://kr8/show_tickets/' id='t3'>Посмотреть проданные билеты</a>
+                <a class='button' href='?exit=true'>Выйти</a>
             </body>
 
             </html>";
@@ -35,14 +35,16 @@ else if(isset($_SESSION['signin_login'])){
             <meta charset='utf-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
             <title>Добро пожаловать!</title>
+            <link href='css/style.css' media='screen' rel='stylesheet'>
+            
         </head>
         <body>
-            <h1 id='t1'>Добро пожаловать в наш кинотеатр!</h1>
-            <a href='http://kr8/registration'>Добавить сотрудника</a>
-            <a href='https://kr8/movies/' id='t3'>Выбрать фильм</a>
-            <a href='https://kr8/add_movie/' id='t3'>Добавить фильм</a>
-            <a href='https://kr8/show_tickets/' id='t3'>Посмотреть проданные билеты</a>
-            <a href='?exit=true'>Выйти</a>
+            <h1 id='t1'>Корпоративный сайти кинотеатра Prometheus</h1>
+            <a class='button' href='http://kr8/registration'>Добавить сотрудника</a>
+            <a class='button' href='https://kr8/movies/' id='t3'>Выбрать фильм</a>
+            <a class='button' href='https://kr8/add_movie/' id='t3'>Добавить фильм</a><br><br><br>
+            <a class='button' href='https://kr8/show_tickets/' id='t3'>Посмотреть проданные билеты</a>
+            <a class='button' href='?exit=true'>Выйти</a>
         </body>
 
         </html>";
@@ -57,10 +59,11 @@ echo "<!DOCTYPE html>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
     <title>Добро пожаловать!</title>
+    <link href='css/style.css' media='screen' rel='stylesheet'>
 </head>
 <body>
-	<h1 id='t1'>Добро пожаловать в наш кинотеатр!</h1>
-    <a href='https://kr8/signform1/' id='t3'>Войти</a>
+	<h1 id='t1'>Корпоративный сайти кинотеатра Prometheus</h1>
+    <a class='button' href='https://kr8/signform/' id='t3'>Войти</a>
 </body>
 
 </html>";}
